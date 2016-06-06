6066178660d88d4e4d892194825aa17e8b076110
noname <@>
2016-06-06 14:21:22 +0000

    Users を追加。Entity/User.php にパスワードのハッシュ化メソッドを追加。

---
+++ src/Model/Entity/User.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+use Cake\Auth\DefaultPasswordHasher;
+
+/**
+ * User Entity.
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ */
+class User extends Entity
+{
+
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array
+     */
+    protected $_accessible = [
+        '*' => true,
+        'id' => false,
+    ];
+
+    /**
+     * Fields that are excluded from JSON an array versions of the entity.
+     *
+     * @var array
+     */
+    protected $_hidden = [
+        'password'
+    ];
+
+    /**
+     * 引数のパスワードをハッシュ化して返す。
+     * ハッシュで使われる salt は config/app.php: Security/salt を参照。
+     *
+     * @param string $password パスワード文字列
+     * @return string ハッシュ化されたパスワード
+     */
+    protected function _setPassword($password)
+    {
+        return (new DefaultPasswordHasher)->hash($password);
+    }
+}

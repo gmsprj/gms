be0678c27ea6183ef27aa47422ddcaadfe0ca4a5
noname <@>
2016-06-06 20:18:10 +0000

    リファクタリング。

--- src/Controller/BoardsController.php
+++ src/Controller/BoardsController.php
@@ -10,12 +10,12 @@ use DateTime;
 
 class BoardsController extends AppController
 {
-	public function initialize()
-	{
-		parent::initialize();
-		$this->loadComponent('Csrf');
-		$this->viewBuilder()->layout('fwu-default');
-	}
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadComponent('Csrf');
+        $this->viewBuilder()->layout('fwu-default');
+    }
 
     public function beforeFilter(Event $event)
     {
@@ -23,90 +23,88 @@ class BoardsController extends AppController
         $this->Auth->allow(['view', 'post']);
     }
 
-	public function index()
-	{
-		$boards = $this->Boards->find('all');
-		$this->set('boards', $boards);
-	}
+    public function index()
+    {
+        $boards = $this->Boards->find('all');
+        $this->set('boards', $boards);
+    }
 
-	public function view($boardId)
-	{
-		$this->loadModel('Boards');
-		$this->loadModel('Threads');
+    public function view($boardId)
+    {
+        $this->loadModel('Boards');
+        $this->loadModel('Threads');
 
-		$board = $this->Boards->find()
-			->where(['id' => $boardId])
-			->first()
-			;
+        $board = $this->Boards->find()
+            ->where(['id' => $boardId])
+            ->first();
 
-		$threads = $this->Threads->find('all')
-			->where(['board_id' => $boardId])
-			->order(['name' => 'DESC'])
-			;
+        $threads = $this->Threads->find('all')
+            ->where(['board_id' => $boardId])
+            ->order(['name' => 'DESC']);
 
-		$this->set('board', $board);
-		$this->set('threads', $threads);
-	}
+        $this->set('board', $board);
+        $this->set('threads', $threads);
+    }
 
-	public function post()
-	{
-		$threadName = $this->request->data('threadName');
-		$postName = $this->request->data('postName');
-		$postContent = $this->request->data('postContent');
-		$created = new DateTime(date('Y-m-d H:i:s'));
-		$boardId = $this->request->data('boardId');
-		$redirect = ['action' => 'view', $boardId];
-		
-		// スレッドの作成
-		$threadsTable = TableRegistry::get('Threads');
-		$newThread = $threadsTable->newEntity([
-			'name' => $threadName,
-			'created' => $created,
-			'board_id' => $boardId,
-		]);
+    public function post()
+    {
+        $threadName = $this->request->data('threadName');
+        $postName = $this->request->data('postName');
+        $postContent = $this->request->data('postContent');
+        $created = new DateTime(date('Y-m-d H:i:s'));
+        $boardId = $this->request->data('boardId');
+        $redirect = ['action' => 'view', $boardId];
+        
+        // スレッドの作成
+        $threadsTable = TableRegistry::get('Threads');
+        $newThread = $threadsTable->newEntity([
+            'name' => $threadName,
+            'created' => $created,
+            'board_id' => $boardId,
+        ]);
 
         if ($newThread->errors()) {
-			$this->Flash->error(__('入力が不正です。'));
-			Log::write('error', $newThread->toString());
+            $this->Flash->error(__('入力が不正です。'));
+            Log::write('error', $newThread->toString());
             $this->redirect($redirect);
             return;
         }
-		
-		if ($threadsTable->save($newThread)) {
-			Log::write('debug', $newThread->toString());
-		} else {
-			$this->Flash->error(__('登録に失敗しました。'));
-			Log::write('error', $newThread->toString());
+        
+        if ($threadsTable->save($newThread)) {
+            Log::write('debug', $newThread->toString());
+        } else {
+            $this->Flash->error(__('登録に失敗しました。'));
+            Log::write('error', $newThread->toString());
             $this->redirect($redirect);
-			return;
-		}
+            return;
+        }
 
-		// ポストの作成
-		$postsTable = TableRegistry::get('Posts');
-		$newPost = $postsTable->newEntity([
-			'name' => $postName,
-			'content' => $postContent,
-			'thread_id' => $newThread->id,
-		]);
-		
+        // ポストの作成
+        $postsTable = TableRegistry::get('Posts');
+        $newPost = $postsTable->newEntity([
+            'name' => $postName,
+            'content' => $postContent,
+            'thread_id' => $newThread->id,
+        ]);
+        
         if ($newPost->errors()) {
-			$this->Flash->error(__('入力が不正です。'));
-			Log::write('error', $newPost->toString());
-			$threadsTable->delete($newThread);
+            $this->Flash->error(__('入力が不正です。'));
+            Log::write('error', $newPost->toString());
+            $threadsTable->delete($newThread);
             $this->redirect($redirect);
             return;
         }
-		
-		if ($postsTable->save($newPost)) {
-			Log::write('debug', $newPost->toString());
-		} else {
-			$this->Flash->error(__('登録に失敗しました。'));
-			Log::write('error', $newPost->toString());
-			$threadsTable->delete($newThread);
-		}
+        
+        if ($postsTable->save($newPost)) {
+            Log::write('debug', $newPost->toString());
+        } else {
+            $this->Flash->error(__('登録に失敗しました。'));
+            Log::write('error', $newPost->toString());
+            $threadsTable->delete($newThread);
+        }
 
-		$this->redirect($redirect);
-	}
+        $this->redirect($redirect);
+    }
 }
 
 ?>
--- src/Controller/ThreadsController.php
+++ src/Controller/ThreadsController.php
@@ -11,12 +11,12 @@ use DateTime;
 
 class ThreadsController extends AppController
 {
-	public function initialize()
-	{
-		parent::initialize();
-		$this->loadComponent('Csrf');
-		$this->viewBuilder()->layout('fwu-default');
-	}
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadComponent('Csrf');
+        $this->viewBuilder()->layout('fwu-default');
+    }
 
     public function beforeFilter(Event $event)
     {
@@ -24,74 +24,71 @@ class ThreadsController extends AppController
         $this->Auth->allow(['view', 'post']);
     }
 
-	public function index()
-	{
-		$this->autoRender = false;
-		throw new Exception(__('そんなページありません。'));
-	}
+    public function index()
+    {
+        $this->autoRender = false;
+        throw new Exception(__('そんなページありません。'));
+    }
 
-	public function view($threadId)
-	{
-		$this->loadModel('Boards');
-		$this->loadModel('Threads');
-		$this->loadModel('Posts');
+    public function view($threadId)
+    {
+        $this->loadModel('Boards');
+        $this->loadModel('Threads');
+        $this->loadModel('Posts');
 
-		$thread = $this->Threads->find()
-			->where(['id' => $threadId])
-			->first()
-			;
+        $thread = $this->Threads->find()
+            ->where(['id' => $threadId])
+            ->first();
 
-		if ($thread == null) {
-			throw new Exception(__('そんなスレッドありません。'));
-		}
-		
-		$board = $this->Boards->find()
-			->where(['id' => $thread->board_id])
-			->first()
-			;
+        if ($thread == null) {
+            throw new Exception(__('そんなスレッドありません。'));
+        }
+        
+        $board = $this->Boards->find()
+            ->where(['id' => $thread->board_id])
+            ->first();
 
-		$posts = $this->Posts->find('all')
-			->where(['thread_id' => $thread->id])
-			;
+        $posts = $this->Posts->find('all')
+            ->where(['thread_id' => $thread->id]);
 
-		$this->set('board', $board);
-		$this->set('thread', $thread);
-		$this->set('posts', $posts);
-	}
+        $this->set('board', $board);
+        $this->set('thread', $thread);
+        $this->set('posts', $posts);
+    }
 
-	/* TODO: PlazaController.php: post() と重複 */
-	public function post()
-	{
-		$name = $this->request->data('name');
-		$content = $this->request->data('content');
-		$threadId = $this->request->data('threadId');
-		$created = new DateTime(date('Y-m-d H:i:s'));
+    /* TODO: PlazaController.php: post() と重複 */
+    public function post()
+    {
+        $name = $this->request->data('name');
+        $content = $this->request->data('content');
+        $threadId = $this->request->data('threadId');
+        $created = new DateTime(date('Y-m-d H:i:s'));
         $redirect = ['action' => 'view', $threadId];
 
-		// ポストの作成
-		$postsTable = TableRegistry::get('Posts');
-		$newPost = $postsTable->newEntity([
-			'name' => $name,
-			'content' => $content,
-			'thread_id' => $threadId,
-		]);
-		
+        // ポストの作成
+        $postsTable = TableRegistry::get('Posts');
+        $newPost = $postsTable->newEntity([
+            'name' => $name,
+            'content' => $content,
+            'thread_id' => $threadId,
+        ]);
+        
         if ($newPost->errors()) {
-			$this->Flash->error(__('入力が不正です。'));
-			Log::write('error', $newPost->toString());
+            $this->Flash->error(__('入力が不正です。'));
+            Log::write('error', $newPost->toString());
             $this->redirect($redirect);
             return;
         }
 
-		if ($postsTable->save($newPost)) {
-			Log::write('debug', $newPost->toString());
-		} else {
-			$this->Flash->error(__('入力が不正です。'));
-			Log::write('error', $newPost->toString());
-		}
-		
-		$this->redirect($redirect);
-	}
+        if ($postsTable->save($newPost)) {
+            Log::write('debug', $newPost->toString());
+        } else {
+            $this->Flash->error(__('入力が不正です。'));
+            Log::write('error', $newPost->toString());
+        }
+        
+        $this->redirect($redirect);
+    }
 }
 
 ?>

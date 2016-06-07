5aab0621560ae3001d244ca9fae78f59a8d5af23
noname <@>
2016-06-07 14:44:23 +0000

    Plaza の認証ユーザー対応。

--- src/Controller/PlazaController.php
+++ src/Controller/PlazaController.php
@@ -10,88 +10,92 @@ use Exception;
 
 class PlazaController extends AppController
 {
-	public function initialize()
-	{
-		parent::initialize();
-		$this->loadComponent('Csrf');
-		$this->viewBuilder()->layout('fwu-default');
-	}
+    public function initialize()
+    {
+        parent::initialize();
 
-	public function index()
-	{
-		$this->loadModel('Boards');
-		$this->loadModel('Threads');
-		$this->loadModel('Posts');
-		
-		// 板のリスト
-		$boards = $this->Boards->find('all')
-			->order(['name' => 'DESC'])
-			;
-		if ($boards->count() == 0) {
-			throw new Exception('(´･ω･`)板がありません。');
-		}
-		
-		// 表示板
-		$dispBoard = $this->Boards->find()
-			->where(['name' => 'ロビー'])
-			->first()
-			;
-		if ($dispBoard == null) {
-			throw new Exception('(´･ω･`)ロビー板を作成してください。');
-		}
-		
-		// 表示板スレッドリスト
-		$dispThreads = $this->Threads->find('all')
-			->where(['board_id' => $dispBoard->id])
-			;
-		if ($dispThreads->count() == 0) {
-			throw new Exception('(´･ω･`)' . $dispBoard->name . '板にスレッドがありません。最低１スレッド必要です。');
-		}
-		
-		// 表示スレッド
-		$dispThread = $this->Threads->find('all')
-			->where(['board_id' => $dispBoard->id])
-			->first()
-			;
-		
-		// 表示スレッドのポストリスト
-		$dispPosts = $this->Posts->find('all')
-			->where(['thread_id' => $dispThread->id])
-			;
+        $this->loadComponent('Csrf');
+        $this->viewBuilder()->layout('fwu-default');
+        $this->Auth->allow(['index', 'post']);
+        $this->loadModel('Boards');
+        $this->loadModel('Threads');
+        $this->loadModel('Posts');
+    }
 
-		// テンプレートに設定
-		$this->set('boards', $boards);
-		$this->set('dispBoard', $dispBoard);
-		$this->set('dispThreads', $dispThreads);
-		$this->set('dispThread', $dispThread);
-		$this->set('dispPosts', $dispPosts);
-	}
+    public function index()
+    {
+        // 板のリスト
+        $boards = $this->Boards->find('all')
+            ->order(['name' => 'DESC']);
 
-	/* TODO: ThreadsController.php: post() と重複 */
-	public function post()
-	{
-		$name = $this->request->data('name');
-		$content = $this->request->data('content');
-		$threadId = $this->request->data('threadId');
-		$created = new DateTime(date('Y-m-d H:i:s'));
+        if ($boards->count() == 0) {
+            throw new Exception('板がありません。');
+        }
+        
+        // 表示板
+        $dispBoard = $this->Boards->find()
+            ->where(['name' => 'ロビー'])
+            ->first();
 
-		// ポストの作成
-		$postTable = TableRegistry::get('Posts');
-		$newPost = $postTable->newEntity([
-			'name' => $name,
-			'content' => $content,
-			'thread_id' => $threadId,
-		]);
-		
-		if ($postTable->save($newPost)) {
-			Log::write('debug', $newPost->toString());
-		} else {
-			$this->Flash->error('入力が不正です。');
-			Log::write('error', $newPost->toString());
-		}
-		
-		$this->redirect(['action' => 'index']);
-	}
+        if ($dispBoard == null) {
+            throw new Exception('ロビー板を作成してください。');
+        }
+        
+        // 表示板スレッドリスト
+        $dispThreads = $this->Threads->find('all')
+            ->where(['board_id' => $dispBoard->id]);
+
+        if ($dispThreads->count() == 0) {
+            throw new Exception($dispBoard->name . '板にスレッドがありません。最低１スレッド必要です。');
+        }
+        
+        // 表示スレッド
+        $dispThread = $this->Threads->find('all')
+            ->where(['board_id' => $dispBoard->id])
+            ->first();
+        
+        // 表示スレッドのポストリスト
+        $dispPosts = $this->Posts->find('all')
+            ->where(['thread_id' => $dispThread->id]);
+
+        // 認証ユーザーを取得して投稿者ネームを得る
+        $authUser = $this->Auth->user();
+        $postName = ($authUser == null ? '名無しさん' : $authUser['name']);
+
+        // テンプレートに設定
+        $this->set('boards', $boards);
+        $this->set('dispBoard', $dispBoard);
+        $this->set('dispThreads', $dispThreads);
+        $this->set('dispThread', $dispThread);
+        $this->set('dispPosts', $dispPosts);
+        $this->set('postName', $postName);
+    }
+
+    /* TODO: ThreadsController.php: post() と重複 */
+    public function post()
+    {
+        $name = $this->request->data('name');
+        $content = $this->request->data('content');
+        $threadId = $this->request->data('threadId');
+        $created = new DateTime(date('Y-m-d H:i:s'));
+
+        // ポストの作成
+        $postTable = TableRegistry::get('Posts');
+        $newPost = $postTable->newEntity([
+            'name' => $name,
+            'content' => $content,
+            'thread_id' => $threadId,
+        ]);
+        
+        if ($postTable->save($newPost)) {
+            Log::write('debug', $newPost->toString());
+        } else {
+            $this->Flash->error('入力が不正です。');
+            Log::write('error', $newPost->toString());
+        }
+        
+        $this->redirect(['action' => 'index']);
+    }
 }
 
 ?>
